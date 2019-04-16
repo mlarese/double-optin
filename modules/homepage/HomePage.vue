e58988ef6bfcf45b44aff78030f61fe78d5b1d29
mlarese <mauro.larese@gmail.com>
2019-04-16 07:08:05 +0000

    progress

---
+++ modules/homepage/HomePage.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+    <v-layout
+      column
+      justify-center
+      align-center
+    >
+      <v-flex
+        xs5
+        sm5
+        md5>
+        <v-avatar
+          slot="activator"
+          size="100px"
+          class="my-5"
+        >
+          <img
+
+            src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+            alt="Avatar"
+          >
+        </v-avatar>
+      </v-flex>
+      <v-layout
+        class="title"
+        color="red"> {{ $vuetify.t('To you, our warm welcome') }}</v-layout>
+      <br>
+      <strong> {{ $vuetify.t('We care about all the needs of our guests, especially the confidentiality') }}</strong>
+      <span>{{ $vuetify.t(' For this reason, we ask you for permission to process your data according to the privacy regulations listed below ') }}</span>
+      <br>
+      <strong> {{ $vuetify.t(' No spam! You decide how to keep in touch with us. ') }}</strong>
+      <br>
+    </v-layout>
+    <v-layout>
+      <v-layout
+        row>
+        <v-flex
+          xs12
+          sm4
+        >
+          <h4>{{ $vuetify.t('Name*') }}</h4>
+          <v-layout mx-1>
+            <v-text-field
+              :rules="rules.name"
+              required
+              box
+              hide-details
+            />
+          </v-layout>
+
+        </v-flex>
+
+        <v-flex
+          xs12
+          sm4>
+          <h4>{{ $vuetify.t('Surname*') }}</h4>
+          <v-layout mx-1>
+            <v-text-field
+              :rules="surnameRules"
+              :counter="10"
+              required
+              box
+              hide-details
+
+            />
+          </v-layout>
+
+        </v-flex>
+
+        <v-flex
+          xs12
+          sm4>
+          <h4 >{{ $vuetify.t('Email*') }}</h4>
+          <v-layout mx-1>
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              type="email"
+              box
+              hide-details
+              required
+            />
+          </v-layout>
+
+        </v-flex>
+        <v-flex
+          xs12
+          sm4>
+          <h4>{{ $vuetify.t('Phone Number') }}</h4>
+          <v-layout mx-1>
+            <v-text-field
+              v-model="phone"
+              mask="phone"
+              box
+              hide-details
+            />
+          </v-layout>
+
+        </v-flex>
+      </v-layout>
+
+    </v-layout>
+    <br>
+    <strong> {{ $vuetify.t(' Processing of personal data') }}</strong>
+    <v-flex xs12>
+      <v-textarea
+        color="teal"
+        box
+        full-width
+      />
+    </v-flex>
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[(v) => !!v || 'You must agree to continue!']"
+      label="You must agree to continue?"
+      required
+    />
+    <v-checkbox
+      v-model="checkbox1"
+      :rules="[(v) => !!v || 'You must agree to continue!']"
+      label="You must agree to continue?"
+      required
+    />
+    <div>
+      <v-layout
+        justify-center
+        class="my-2">
+        <v-btn
+          href="/thankyou"
+          color="blue"
+          dark
+          large>
+          {{ $vuetify.t('Send Your Data') }}
+        </v-btn>
+      </v-layout>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+      data () {
+          return {
+              rules: {
+                  name: [v => !!v || this.$vuetify.t('required')]
+              },
+              surnameRules: [
+                  (v) => !!v || 'Name is required',
+                  (v) => v && v.length <= 10 || 'Name must be less than 10 characters'
+              ],
+              emailRules: [
+                  (v) => !!v || 'E-mail is required',
+                  (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+              ],
+              checkbox: false,
+              checkbox1: false
+          }
+      }
+  }
+</script>
---
+++ pages/thankyou/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <v-layout
+      column
+      justify-center
+      align-center
+    >
+      <v-flex
+        xs5
+        sm5
+        md5>
+        <v-avatar
+          slot="activator"
+          size="100px"
+          class="my-5"
+        >
+          <img
+
+            src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+            alt="Avatar"
+          >
+        </v-avatar>
+      </v-flex>
+      <v-layout
+        class="title"
+        color="red"> {{ $vuetify.t('Thank You') }} {{ Mauro }}!</v-layout>
+      <br>
+      <span>{{ $vuetify.t(' We sent you an email. ') }}</span>
+
+      <strong> {{ $vuetify.t(' PLease, answer to confirm your personal details ') }}</strong>
+    </v-layout>
+    <v-layout
+      column
+      justify-center
+      align-center
+    >
+      <v-flex
+        xs5
+        sm5
+        md5>
+        <v-icon
+          slot="activator"
+          size="100px"
+          class="my-5"
+          color="blue"
+        >
+          email
+        </v-icon>
+      </v-flex>
+    </v-layout>
+    <div>
+      <v-layout
+        justify-center
+        class="my-2">
+        <v-btn
+          href="/confirmationemail"
+          flat
+          large>{{ $vuetify.t('Return to HomePage') }}
+        </v-btn>
+      </v-layout>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Index"
+    }
+</script>
+
+<style scoped>
+
+</style>
